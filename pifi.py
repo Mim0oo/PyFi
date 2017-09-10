eb096f4aa0bc4ff52f1d9fe6924d40619c2f6544
Martin Georgiev <geeorgiev@gmail.com>
2017-09-10 23:49:34 +0300

    Initial commit with PiFi

---
+++ pifi.py
@@ -0,0 +1,62 @@
+print "-------------------------------------"
+print " PyFi - Python WiFi Monitor v1.0:10.09.2017"
+print " This applications restarts WiFi adapter on fail"
+print " By Martin Georgiev email: geeorgiev[at]gmail.com"
+print " All rights reserved, GNU General Public License v3.0"
+print "----------------------------------------------------"
+print "loading assets...",
+import subprocess
+import requests
+import logging
+from time import sleep
+from datetime import datetime
+import threading
+print " done"
+print "------------------------------------------"
+
+# Debug configuration
+logging.basicConfig(filename="Wifi.log", level=logging.DEBUG)
+
+
+def run_win_cmd(cmd):
+    result = []
+    process = subprocess.Popen(cmd,
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    for line in process.stdout:
+        result.append(line)
+    errcode = process.returncode
+    for line in result:
+        print(line)
+    if errcode is not None:
+        raise Exception('cmd %s failed, see above for details', cmd)
+
+
+def ping_alive():
+    try:
+        r = requests.get('http://google.com')
+    except Exception, e:
+        logging.debug(clock+"Could not restart adapter Wifi.")
+        return 500
+    return r.status_code
+
+
+def restart_wifi_adapter():
+    run_win_cmd('netsh interface set interface "WiFi" admin=disable')
+    sleep(3)
+    run_win_cmd('netsh interface set interface "WiFi" admin=enable')
+
+
+def loop_mon():
+    test = ping_alive()
+    if test != 200:
+        # Current local time getter
+        clock = datetime.now().strftime('[%Y-%m-%d %H:%M]')
+        # Logging and restart
+        logging.debug(clock+" Restarting adapter.")
+        print clock+" Restarting adapter..."
+        restart_wifi_adapter()
+    threading.Timer(5, loop_mon).start()
+
+loop_mon()
